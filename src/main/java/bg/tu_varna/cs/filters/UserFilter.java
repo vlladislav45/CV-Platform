5781105a7a103d741963bcef4f277166b35dc522
Vladislav <vlladislav45@gmail.com>
2020-04-16 21:50:32 +0300

    Updated User Filter

---
+++ src/main/java/bg/tu_varna/cs/filters/UserFilter.java
@@ -0,0 +1,35 @@
+package bg.tu_varna.cs.filters;
+
+import bg.tu_varna.cs.domain.entities.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author Vladislav Enev
+ *
+ */
+@WebFilter("/index")
+public class UserFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        HttpSession session = req.getSession(false);
+        User sessionUser = (session != null)
+                ? (User)session.getAttribute("user")
+                : null;
+
+        if (sessionUser == null && req.getParameter("user") != null) {
+            res.sendRedirect("index");
+        } else {
+            filterChain.doFilter(req,res);
+        }
+    }
+}
